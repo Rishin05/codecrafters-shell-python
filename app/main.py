6f007396c9aafd669b5cf2374a923db65d5a4ef2
Rishin05 <rishinp26@gmail.com>
2025-02-24 21:46:35 -0500

    type bulletin

--- app/main.py
+++ app/main.py
@@ -10,6 +10,12 @@ def main():
             sys.exit(0)
         if command.startswith("echo "):
             print(command[5:])
+        elif command.startswith("type "):
+            cmd_name = command[5:]
+            if cmd_name in {"echo", "exit","type"}:
+                print(f"{cmd_name} is a shell builtin")
+            else:
+                print(f"{cmd_name}: not found")
         else:
             print(f"{command}: command not found")
 
