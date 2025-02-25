d550bccb57bcafcf9a1f0ccff8f502f488b2b57b
Rishin05 <rishinp26@gmail.com>
2025-02-24 21:56:23 -0500

    exec files

--- app/main.py
+++ app/main.py
@@ -1,24 +1,33 @@
 import sys
+import shutil
 
+BUILTIN_CMD = {"exit", "echo", "type"}
+
+def type_cmd(command):
+    if command in BUILTIN_CMD:
+        print(f"{command} is a shell builtin")
+    elif (path := shutil.which(command)):
+        print(f"{command} is {path}")
+    else:
+        print(f"{command}: not found")
 
 def main():
-    # Uncomment this block to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit 0":
-            sys.exit(0)
-        if command.startswith("echo "):
-            print(command[5:])
-        elif command.startswith("type "):
-            cmd_name = command[5:]
-            if cmd_name in {"echo", "exit","type"}:
-                print(f"{cmd_name} is a shell builtin")
-            else:
-                print(f"{cmd_name}: not found")
-        else:
-            print(f"{command}: command not found")
+        sys.stdout.flush()
+        command = input().split()
 
+        if not command:
+            continue  # Skip empty input
+
+        if command[0] == "exit" and len(command) == 2 and command[1] == "0":
+            exit()
+        elif command[0] == "echo":
+            print(" ".join(command[1:]))
+        elif command[0] == "type" and len(command) == 2:
+            type_cmd(command[1])
+        else:
+            print(f"{command[0]}: command not found")
 
 if __name__ == "__main__":
     main()
