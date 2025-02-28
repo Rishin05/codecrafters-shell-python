7169fe1b4950b0446eaf14232112ffee1ecc1937
Rishin05 <rishinp26@gmail.com>
2025-02-28 15:26:23 -0500

    redirect stdout

--- app/main.py
+++ app/main.py
@@ -1,100 +1,97 @@
 import sys
 import os
 import subprocess
-def mysplit(input):
-    res = [""]
-    current_quote = ""
+def split_input(inp):
     i = 0
-    while i < len(input):
-        c = input[i]
-        if c == "\\":
-            ch = input[i + 1]
-            if current_quote == "'":
-                res[-1] += c
-            elif current_quote == '"':
-                ch = input[i + 1]
-                if ch in ["\\", "$", '"', "\n"]:
-                    res[-1] += ch
+    inpList = []
+    toFile = ""
+    curWord = ""
+    while i < len(inp):
+        if inp[i] == "\\":
+            curWord += inp[i + 1]
+            i += 1
+        elif inp[i] == " ":
+            if ">" in curWord:
+                toFile = inp[i + 1 :]
+                return inpList, toFile
+            if curWord:
+                inpList.append(curWord)
+            curWord = ""
+        elif inp[i] == "'":
+            i += 1
+            while inp[i] != "'":
+                curWord += inp[i]
+                i += 1
+        elif inp[i] == '"':
+            i += 1
+            while inp[i] != '"':
+                if inp[i] == "\\" and inp[i + 1] in ["\\", "$", '"']:
+                    curWord += inp[i + 1]
+                    i += 2
                 else:
-                    res[-1] += "\\" + ch
-                i += 1
-            else:
-                res[-1] += input[i + 1]
-                i += 1
-        elif c in ['"', "'"]:
-            if current_quote == "":
-                current_quote = c
-            elif current_quote == c:
-                current_quote = ""
-            else:
-                res[-1] += c
-        elif c == " " and current_quote == "":
-            if res[-1] != "":
-                res.append("")
+                    curWord += inp[i]
+                    i += 1
         else:
-            res[-1] += c
+            curWord += inp[i]
         i += 1
-    if res[-1] == "":
-        res.pop()
-    return res
-def get_user_command():
-    sys.stdout.write("$ ")
-    out, err = sys.stdout, sys.stderr
-    inp = mysplit(input())
-    if "1>" in inp:
-        idx = inp.index("1>")
-        inp, out = inp[:idx], inp[idx + 1]
-    elif ">" in inp:
-        idx = inp.index(">")
-        inp, out = inp[:idx], inp[idx + 1]
-    return inp, out, err
-def get_file(dirs, filename):
-    for dir in dirs:
-        filepath = f"{dir}/{filename}"
-        if os.path.isfile(filepath):
-            return filepath
-    return None
-def handle_command(inp, dirs, HOME, out, err):
-    toCloseOut = False
-    if type(out) is str:
-        toCloseOut = True
-        out = open(out, "w+")
-    match inp:
-        case ["exit", "0"]:
-            sys.exit(0)
-        case ["echo", *args]:
-            out.write(" ".join(args) + "\n")
-        case ["type", arg]:
-            if arg in ["type", "exit", "echo", "pwd", "cd"]:
-                out.write(f"{arg} is a shell builtin\n")
-            elif filepath := get_file(dirs, arg):
-                out.write(f"{arg} is {filepath}\n")
-            else:
-                out.write(f"{arg}: not found\n")
-        case ["pwd"]:
-            out.write(f"{os.getcwd()}\n")
-        case ["cd", "~"]:
-            os.chdir(HOME)
-        case ["cd", path]:
-            if os.path.isdir(path):
-                os.chdir(path)
-            else:
-                err.write(f"cd: {path}: No such file or directory\n")
-        case [file, *args]:
-            if filepath := get_file(dirs, file):
-                subprocess.run([filepath, *args], stdout=out, stderr=err)
-            else:
-                err.write(f"{' '.join(inp)}: command not found\n")
-    if toCloseOut:
-        out.close()
-def main(dirs, HOME):
-    # Uncomment this block to pass the first stage
-    # Wait for user input
-    while True:
-        inp, out, err = get_user_command()
-        handle_command(inp, dirs, HOME, out, err)
+    inpList.append(curWord)
+    return inpList, toFile
+def main():
+    exited = False
+    path_list = os.environ["PATH"].split(":")
+    builtin_list = ["exit", "echo", "type", "pwd", "cd"]
+    while not exited:
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
+        # Wait for user input
+        userinp = input()
+        inpList, toFile = split_input(userinp)
+        output = ""
+        match inpList[0]:
+            case "cd":
+                path = inpList[1]
+                if path == "~":
+                    os.chdir(os.environ["HOME"])
+                elif os.path.isdir(path):
+                    os.chdir(path)
+                else:
+                    output = path + ": No such file or directory"
+            case "pwd":
+                output = os.getcwd()
+            case "type":
+                for path in path_list:
+                    if os.path.isfile(f"{path}/{inpList[1]}"):
+                        output = inpList[1] + " is " + f"{path}/{inpList[1]}"
+                        break
+                if inpList[1] in builtin_list:
+                    output = inpList[1] + " is a shell builtin"
+                if not output:
+                    output = inpList[1] + ": not found"
+            case "echo":
+                output = " ".join(inpList[1:])
+            case "exit":
+                exited = True
+           case _:
+    isCmd = False
+    for path in path_list:
+        p = f"{path}/{inpList[0]}"
+        if os.path.isfile(p):
+            # Execute with the full path, but don't pass it as the first argument
+            proc = subprocess.run(
+                [p] + inpList[1:], 
+                stdout=subprocess.PIPE, 
+                text=True
+            )
+            output = proc.stdout.rstrip()
+            isCmd = True
+            break
+    if not isCmd:
+        output = userinp + ": command not found"
+        if not toFile:
+            if output:
+                print(output, file=sys.stdout)
+        else:
+            with open(toFile, "a") as f:
+                print(output, end="", file=f)
 if __name__ == "__main__":
-    PATH = os.environ.get("PATH")
-    dirs = PATH.split(":")
-    HOME = os.environ.get("HOME")
-    main(dirs, HOME)
+    main()
